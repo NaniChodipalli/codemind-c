fcfb119c372d74672992b7c39ae34ad910b16e6f
NaniChodipalli <97425897+NaniChodipalli@users.noreply.github.com>
2022-04-30 15:14:50 +0530

    #include<stdio.h> int main() {     double r,area;     scanf("%lf",&r);     area=3.14*r*r;     printf("%.2lf",area); }

--- Area_of_a_circle.c
+++ Area_of_a_circle.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int r;
-    double Area;
-    scanf("%d",&r);
-    Area=3.14*r*r;
-    printf("%.2f",Area);
-    return 0;
+    double r,area;
+    scanf("%lf",&r);
+    area=3.14*r*r;
+    printf("%.2lf",area);
 }
